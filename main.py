ef3f3d4d5f44dfd4b237c1c075fc27a9fe356e79
onlyakarsh@gmail.com <onlyakarsh@gmail.com>
2023-05-20 22:44:24 +0530

    Added Main Driver Code

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+
+def draw_logo():
+    f = open('logo.txt', 'r', encoding="utf8")
+    print("")
+    print(''.join([line for line in f]))
+    print("")
+    print("")
+
+
+os.system('color 02')
+draw_logo()
+input()
