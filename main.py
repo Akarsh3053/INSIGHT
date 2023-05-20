e3fa635fcdc67f945fc2e7b77edb41a9629c0e87
onlyakarsh@gmail.com <onlyakarsh@gmail.com>
2023-05-20 23:07:00 +0530

    Updated Driver Code Options.

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def draw_logo():
 os.system('color 02')
 draw_logo()
 
-print("\n\n\n\n\n 1. IP Lookup")
-x = int(input('Enter your Choice : '))
+print("\n\n\n 1. IP Lookup")
+x = int(input('\n\nEnter your Choice : '))
 
 if x == 1:
     iptool.ipFinder()
